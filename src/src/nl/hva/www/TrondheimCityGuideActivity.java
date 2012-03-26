7ee000548009f3241ad6b2a1844b4c0e424afee3
Marlon Etheredge <marlon.etheredge@gmail.com>
2012-03-26 15:49:30 +0200

    Initial commit containing empty project

---
+++ src/src/nl/hva/www/TrondheimCityGuideActivity.java
@@ -0,0 +1,13 @@
+package nl.hva.www;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TrondheimCityGuideActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
