318b925c0c6a4125391fbfdb17fd21e4e624cc66
Rikkert <hjtenklooster@gmail.com>
2012-04-17 08:38:38 +0200

    Maps toegevoegd zodat de AVD ofzo getest kan worden en je de library van gmaps kan testen.

--- GUIProject/src/dirk/test/DirkActivity.java
+++ GUIProject/src/dirk/test/DirkActivity.java
@@ -1,25 +1,88 @@
 package dirk.test;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 
-import android.app.Activity;
-import android.os.Bundle;
-
 public class DirkActivity extends MapActivity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.map );
-        MapView mapView = (MapView) findViewById(R.id.mapview);
-        mapView.setBuiltInZoomControls(true);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.map);
+		MapView mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+
+		LocationManager mlocManager =
+
+		(LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		LocationListener mlocListener = new MyLocationListener();
+
+		mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+				mlocListener);
+
+	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
+	public class MyLocationListener implements LocationListener {
+
+		@Override
+		public void onLocationChanged(Location loc) {
+			loc.getLatitude();
+
+			loc.getLongitude();
+
+			String Text = "My current location is: " +
+
+			"Latitud = " + loc.getLatitude() +
+
+			"Longitud = " + loc.getLongitude();
+
+			Toast.makeText(getApplicationContext(), Text, Toast.LENGTH_SHORT)
+					.show();
+		}
+
+		@Override
+		public void onProviderDisabled(String provider)
+
+		{
+
+			Toast.makeText(getApplicationContext(), "Gps Disabled",
+					Toast.LENGTH_SHORT).show();
+
+		}
+
+		@Override
+		public void onProviderEnabled(String provider)
+
+		{
+
+			Toast.makeText(getApplicationContext(),
+
+			"Gps Enabled",
+
+			Toast.LENGTH_SHORT).show();
+
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras)
+
+		{
+
+		}
+
+	}/* End of Class MyLocationListener */
 }
---
+++ GUIProject/src/dirk/test/MyLocationListener.java
@@ -0,0 +1,7 @@
+package dirk.test;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.widget.Toast;
+
